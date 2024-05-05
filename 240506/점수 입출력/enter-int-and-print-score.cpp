c33c3574a768c339e00434a538b0c7be669087f7
h22calm <167616872+h22calm@users.noreply.github.com>
2024-05-06 06:28:26 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    1<=n<=1000;
+    cin>>n;
+    cout<<"Your score is"<<" "<<n<<" "<<"point.";
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
