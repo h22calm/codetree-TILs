0d5a05618ddabf4ae64e3039a69342a7143cf68b
h22calm <167616872+h22calm@users.noreply.github.com>
2024-05-06 06:46:58 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double n;
+    
+    cin >> n;
+    cout << fixed;
+    cout.precision(1);
+    
+    cout << n * 30.48;
+
+   
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
