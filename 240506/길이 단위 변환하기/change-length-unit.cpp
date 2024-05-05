fba06788666a01b8a46f9711f48c90ccfba7c8db
h22calm <167616872+h22calm@users.noreply.github.com>
2024-05-06 02:28:17 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240506/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    
+    float a=280.4;
+    string b="209214.2";
+    
+    cout<<"9.2ft"<<" "<<"="<<" "<<a<<"cm"<<endl<<"1.3mi"<<" "<<"="<<" "<<b<<"cm";
+    
+    return 0;
+}
